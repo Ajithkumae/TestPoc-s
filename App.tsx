b75946039aab57404433f0d6843efb99db056c29
Ajith Kumar <ajith007rocks@gmail.com>
2023-04-21 14:12:09 +0530

    initial push

---
+++ App.tsx
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View style={styles.container}>
+          <View style={{flex: 1}}>
+            <Text>Lefzzzzxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxt</Text>
+          </View>
+          <View style={styles.center}>
+            <Text>Center</Text>
+          </View>
+          <View style={{flex: 1, alignItems: 'flex-end'}}>
+            <Text>Right</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
